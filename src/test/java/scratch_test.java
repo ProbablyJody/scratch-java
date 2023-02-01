814bf489942a5abca64198a19a2ef2869bbdf0fa
Jody Alford <ProbablyJody@users.noreply.github.com>
2023-02-01 11:46:24 -0600

    updated test

--- src/test/java/scratch_test.java
+++ src/test/java/scratch_test.java
@@ -1,8 +1,35 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 class ScratchTest {
     @Test
     void scratch() {
+
+    System.out.println("Primitive - Return:" + primitiveTest());
+
+    }
+
+    int primitiveTest() {
+        int primitive_example = 1;
+
+        try {
+
+            System.out.println("Primitive - Init: " + primitive_example);
+            throw new RuntimeException("re");
+
+        }  catch (RuntimeException re) {
+            System.out.println("Primitive - Catch: " + primitive_example);
+            return primitive_example;
+
+        } finally {
+            primitive_example = 2;
+            System.out.println("Primitive - Finally: " + primitive_example);
+            return primitive_example;
+        }
 
     }
 }
